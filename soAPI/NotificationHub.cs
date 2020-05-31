54755b5b182b77f10768ee6a1bbde6253cb0bf85
Ricardo Araújo <ricardo.araujo.t0114627@edu.atec.pt>
2020-05-31 02:53:30 +0100

    initial commit

---
+++ soAPI/NotificationHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace soAPI
+{
+    public class NotificationHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+    }
+}
